f83f086c87a923591923fb3f7a2671906df15ce6
Akihide Ito <akky.supernova@gmail.com>
2021-12-10 19:43:26 +0900

    feat: oauth add 42

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const config = require('./config.js');
 const { syncBuiltinESMExports } = require("module");
+const config = require("./config.js");
 const puppeteer = require("puppeteer");
 
 (async() => {
@@ -15,18 +15,36 @@ const puppeteer = require("puppeteer");
 
     await page.evaluate(() => console.log(`url is ${location.href}`));
 
-    await page.type("#user_login", config.loginConfig.userName);
-    await page.type("#user_password", config.loginConfig.password);
-    const submitButtonDivs = await page.$$(".form-actions");
-    console.log(submitButtonDivs);
-    for (const submitButtonDiv of submitButtonDivs) {
-        const submitButton = await submitButtonDiv.$(".btn");
-        console.log(submitButton);
-        if (submitButton) {
-            await submitButton.click();
-        }
+    console.log(config.loginConfig);
+    await page.type("#user_login", config.loginConfig.tokyo_42.userName);
+    await page.type("#user_password", config.loginConfig.tokyo_42.password);
+    const submitButtonDiv = await page.$(".form-actions");
+    const submitButton = await submitButtonDiv.$(".btn");
+    console.log(submitButton);
+    if (submitButton) {
+        await Promise.all([
+            page.waitForNavigation({
+                waitUntil: ["load", "networkidle2"],
+            }),
+            submitButton.click(),
+        ]);
     }
 
+    console.log("-----------login success------------");
+
+    const authorizeButtonDiv = await page.$(".actions");
+    const authorizeButton = await authorizeButtonDiv.$(".btn-success");
+    console.log(authorizeButton);
+    if (authorizeButton) {
+        await Promise.all([
+            page.waitForNavigation({
+                waitUntil: ["load", "networkidle2"],
+            }),
+            authorizeButton.click(),
+        ]);
+    }
+
+    console.log("-----------OAuth success------------");
     await page.waitFor(500000);
 
     await browser.close();
