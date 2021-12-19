795a5726982dad4691257686652dbbf0d53b856e
Akihide Ito <akky.supernova@gmail.com>
2021-12-19 09:10:07 +0000

    fix to waiting for loading

--- main.js
+++ main.js
@@ -1,84 +1,109 @@
-require("dotenv").config();
+const envPath = "/home/ubuntu/Documents/intra-verify-42tokyo/.env"
+require("dotenv").config({path: envPath});
 const { syncBuiltinESMExports } = require("module");
 const puppeteer = require("puppeteer");
+const { createLogger, format, transports } = require("winston");
+const { splat, combine, timestamp, label, printf, simple } = format;
 
-(async () => {
-  const browser = await puppeteer.launch();
-  // const browser = await puppeteer.launch({
-  //     headless: false,
-  //     slowMo: 10,
-  // });
-  const page = await browser.newPage();
-  await page.goto("https://discord.42tokyo.jp/");
+const myFormat = printf( ({ level, message, timestamp }) => {
+  return `${timestamp} ${level}: ${message}`;
+});
 
-  page.on("console", (msg) => console.log("PAGE LOG:", msg.text()));
+const logger = createLogger(
+	{
+		format: combine(
+			label({message: true}),
+			timestamp(),
+			myFormat
+		),
+		transports: [
+			new transports.Console(),
+			new transports.File({filename: 'result.log'})
+		]
+	}
+);
 
-  await page.evaluate(() => console.log(`url is ${location.href}`));
-
-  await page.type("#user_login", process.env.TOKYO_42_USERNAME);
-  await page.type("#user_password", process.env.TOKYO_42_PASSWORD);
-  const submitButtonDiv = await page.$(".form-actions");
-  const submitButton = await submitButtonDiv.$(".btn");
-  if (submitButton) {
+(async() => {
+    logger.warn('start');
+    const browser = await puppeteer.launch({
+	    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    });
+    // const browser = await puppeteer.launch({
+    //     headless: false,
+    //     slowMo: 10,
+    // });
+    const page = await browser.newPage();
     await Promise.all([
-      page.waitForNavigation({
-        waitUntil: ["load", "networkidle2"],
-      }),
-      submitButton.click(),
-    ]);
-  }
+            page.waitForSelector('#user_login'),
+            page.goto("https://discord.42tokyo.jp/"),
+        ]);
 
-  console.log("-----------login success------------");
-
-  const authorizeButtonDiv = await page.$(".actions");
-  const authorizeButton = await authorizeButtonDiv.$(".btn-success");
-  if (authorizeButton) {
-    await Promise.all([
-      page.waitForNavigation({
-        waitUntil: ["load", "networkidle2"],
-      }),
-      authorizeButton.click(),
-    ]);
-  }
-
-  console.log("-----------OAuth success------------");
-
-  const discordLoginFormDivs = await page.$$(".inputWrapper-31_8H8");
-
-  for (const [i, discordLoginFormDiv] of discordLoginFormDivs.entries()) {
-    console.log(`${i}th discordLoginFormDiv`);
-    const discordLoginForm = await discordLoginFormDiv.$("input");
-    if (i == 0) {
-      await discordLoginForm.type(process.env.DISCORD_EMAIL);
-    } else {
-      await discordLoginForm.type(process.env.DISCORD_PASSWORD);
+    await page.type("#user_login", process.env.TOKYO_42_USERNAME);
+    await page.type("#user_password", process.env.TOKYO_42_PASSWORD);
+    const submitButtonDiv = await page.$(".form-actions");
+    const submitButton = await submitButtonDiv.$(".btn");
+    if (submitButton) {
+        await Promise.all([
+            page.waitForNavigation({
+                waitUntil: ["load", "networkidle2"],
+            }),
+            submitButton.click(),
+        ]);
     }
-  }
 
-  const discordLoginButton = await page.$(".button-3k0cO7");
-  if (discordLoginButton) {
-    await Promise.all([
-      page.waitForNavigation({
-        waitUntil: ["load", "networkidle2"],
-      }),
-      page.waitForSelector("div.footer-3ZalXG"),
-      discordLoginButton.click(),
-    ]);
-  }
+    logger.info("-----------login success------------");
 
-  console.log("-----------discord login success------------");
-  const discordAuthButtonDiv = await page.$(".footer-3ZalXG");
-  console.log(discordAuthButtonDiv);
-  const discordAuthButton = await discordAuthButtonDiv.$(".lookFilled-1Gx00P");
+    const authorizeButtonDiv = await page.$(".actions");
+    const authorizeButton = await authorizeButtonDiv.$(".btn-success");
+    if (authorizeButton) {
+        await Promise.all([
+            page.waitForNavigation({
+                waitUntil: ["load", "networkidle2"],
+            }),
+            authorizeButton.click(),
+        ]);
+    }
 
-  console.log(discordAuthButton);
-  if (discordAuthButton) {
-    await Promise.all([
-      page.waitForNavigation({
-        waitUntil: ["load", "networkidle2"],
-      }),
-      discordAuthButton.click(),
-    ]);
-  }
-  await browser.close();
+    logger.info("-----------OAuth success------------");
+
+    const discordLoginFormDivs = await page.$$(".inputWrapper-31_8H8");
+
+    for (const [i, discordLoginFormDiv] of discordLoginFormDivs.entries()) {
+        const discordLoginForm = await discordLoginFormDiv.$("input");
+        if (i == 0) {
+            await discordLoginForm.type(process.env.DISCORD_EMAIL);
+        } else {
+            await discordLoginForm.type(process.env.DISCORD_PASSWORD);
+        }
+    }
+
+    const discordLoginButton = await page.$(".button-3k0cO7");
+    if (discordLoginButton) {
+        await Promise.all([
+            page.waitForNavigation({
+                waitUntil: ["load", "networkidle2"],
+            }),
+            page.waitForSelector("div.footer-3ZalXG"),
+            discordLoginButton.click(),
+        ]);
+    }
+
+    logger.info("-----------discord login success------------");
+    const discordAuthButtonDiv = await page.$(".footer-3ZalXG");
+    const discordAuthButton = await discordAuthButtonDiv.$(".lookFilled-1Gx00P");
+
+    if (discordAuthButton) {
+        await Promise.all([
+            page.waitForNavigation({
+                waitUntil: ["load", "networkidle2"],
+            }),
+            discordAuthButton.click(),
+        ]);
+    }
+
+
+    logger.info("-----------discord OAuth success------------");
+    logger.warn('finish');
+    
+    await browser.close();
 })();
